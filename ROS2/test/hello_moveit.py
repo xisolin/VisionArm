a3428699477e8da5aee6b1ddc84c619d6e84851d
xisolin <129922963+xisolin@users.noreply.github.com>
2026-01-14 22:28:53 +0800

    增加一点代码量

--- ROS2/test/hello_moveit.py
+++ ROS2/test/hello_moveit.py
@@ -37,6 +37,7 @@ def main():
     if plan_result:
         print("规划成功，开始执行！")
         print("规划成功，开始执行！")
+        print("规划成功，开始执行！")
         vision_arm.execute(plan_result.trajectory,controllers=[])
     else :
         print("规划失败")
